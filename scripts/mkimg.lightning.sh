c0ccc0c940076879922ab357c44ecdad50e7b12c
Jeff Vandrew Jr <jeffvandrew@yahoo.com>
2019-03-03 12:10:06 -0500

    new profile

---
+++ scripts/mkimg.lightning.sh
@@ -0,0 +1,8 @@
+profile_lightning() {
+    profile_extended
+    profile_abbrev="lght"
+    title="LightningOS"
+    desc="Converts old hardware to a 24/7 hardware node."
+    arch="x86_64"
+    apks="$apks avahi git networkmanager docker docker-compose dbus udisks2"
+}
